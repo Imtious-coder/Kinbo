47d646ba15b64821fc0be3ec9ea7d6b89b86ebf0
Imtious-Coder <iarafat386@gmail.com>
2024-03-16 09:38:48 +0600

    :truck: Routes setup completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
+import Product from './pages/Product';
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategory category="men" />} />
+        <Route path="/womens" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kid" />} />
+        <Route path="/product" element={<Product />} />
+        <Route path=":productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="/" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
