8d3daf9cc2d853e0b0aa9c6d753134c1f132ace0
Imtious-Coder <iarafat386@gmail.com>
2024-06-05 22:36:36 +0600

    :art: Popular products, New collections API added.

--- src/components/NewCollections.jsx
+++ src/components/NewCollections.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import new_collection_data from "../assets/new_collections";
+import React, { useEffect, useState } from "react";
+// import new_collection_data from "../assets/new_collections";
 import "../styles/NewCollections.css";
 import Item from "./Item";
 
 const NewCollections = () => {
+  const [newCollection, setNewCollection] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/newcollection")
+      .then((res) => res.json())
+      .then((data) => setNewCollection(data));
+  }, []);
+  
   return (
     <section className="newCollections">
       <h1>NEW COLLECTIONS</h1>
       <div className="line"> {"n"}</div>
       <div className="collections">
-        {new_collection_data.map(
+        {newCollection.map(
           ({ id, name, image, new_price, old_price }, index) => {
             return (
               <Item
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,26 +1,36 @@
-import React from "react";
-import ProductData from "../assets/data";
+import React, { useEffect, useState } from "react";
+// import ProductData from "../assets/data";
 import "../styles/Popular.css";
 import Item from "./Item";
 
 const Popular = () => {
+  const [popularProduct, setPopularProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/popularInWomen")
+      .then((res) => res.json())
+      .then((data) => setPopularProduct(data));
+  }, []);
+  
   return (
     <section className="popular">
       <h1>POPULAR IN WOMEN</h1>
       <div className="line"> {"n"}</div>
       <div className="popular-item">
-        {ProductData.map(({ id, name, image, new_price, old_price }, index) => {
-          return (
-            <Item
-              key={index}
-              id={id}
-              name={name}
-              image={image}
-              new_price={new_price}
-              old_price={old_price}
-            />
-          );
-        })}
+        {popularProduct.map(
+          ({ id, name, image, new_price, old_price }, index) => {
+            return (
+              <Item
+                key={index}
+                id={id}
+                name={name}
+                image={image}
+                new_price={new_price}
+                old_price={old_price}
+              />
+            );
+          }
+        )}
       </div>
     </section>
   );
